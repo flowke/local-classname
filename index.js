af2fd8a8a5959915d91ed298822dbb7af6c9ec70
春田石雨 <flowke@163.com>
2019-01-14 12:40:55 +0800

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function classNameLocals(localsArr, str){
+
+  if (typeof str !== 'string') return str
+  if (Object.prototype.toString.call(localsArr) !== '[object Array]'){
+    console.warn('argument[0] is not a Array, str won\'t be handle parsed')
+    return str
+  }
+  
+  return str.split(' ').reduce(function(accu, name) {
+    if(!name) return accu
+    
+    var localName = find(localsArr, name)
+    if (localName) accu.push(localName)
+    return accu
+    
+  },[]).join(' ')
+  
+}
+
+function find(arr, name){
+  var str;
+  for(var i = arr.length-1; i>=0; i--){
+    if(arr[i][name]){
+      str = arr[i][name]
+      break;
+    }
+  }
+  return str
+}
+
+module.exports = classNameLocals
+module.exports.default = classNameLocals
---
+++ index.test.js
@@ -0,0 +1,34 @@
+const localsName = require('./index')
+
+let str1 = '  as1   as2   as3   as4  '
+let str2 = '  as1 '
+let str3 = 'as1 as5'
+
+let arr = [
+  {
+    as1: 'iam_sa1',
+    as2: 'iam_as2',
+    as5: 'iam_as5_0'
+  },
+  {
+    as3: 'iam_sa3',
+    as4: 'iam_as4',
+    as5: 'iam_as5_1'
+  },
+]
+
+
+test('str has more then one white space within names', ()=>{
+  expect(localsName([arr[0]], str1)).toBe('iam_sa1 iam_as2')
+})
+
+test('have mutiple locals Object', ()=>{
+  expect(localsName(arr, str1)).toBe('iam_sa1 iam_as2 iam_sa3 iam_as4')
+})
+test('last one locals Object matched', ()=>{
+  expect(localsName(arr, str2)).toBe('iam_sa1')
+})
+
+test('the last locals Object priorly', ()=>{
+  expect(localsName(arr, str3)).toBe('iam_sa1 iam_as5_1')
+})
